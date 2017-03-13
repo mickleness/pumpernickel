a32e2a612f0480d902e2dab9c98726bd9079dc52
jeremy <jeremy.wood@mac.com>
2017-03-12 22:57:58 -0400

    Making Layer an inner class

--- pump-data/src/main/java/com/pump/data/scrambler/Scrambler.java
+++ pump-data/src/main/java/com/pump/data/scrambler/Scrambler.java
@@ -315,6 +315,207 @@ public class Scrambler {
 		}
 	}
 	
+	protected static class Layer extends ByteEncoder {
+		
+		private enum RunType { NO_MARKER, STARTING_MARKER, BOTH_MARKERS };
+		
+		/** This is a way of reordering bytes that when invoked twice results in the
+		 * original data.
+		 */
+		protected enum ReorderType {
+
+			/** "ABCDEF" is encoded as "ABCDEF" */
+			NORMAL() {
+				@Override
+				protected void reorder(List<Integer> srcList, int srcPos,
+						int length, int[] dest, int destPos) {
+					for(int a = 0; a<length; a++) {
+						dest[destPos+a] = srcList.get(srcPos+a);
+					}
+				}
+			},
+
+			/** "ABCDEF" is encoded as "FEDCBA" */
+			REVERSE() {
+				@Override
+				protected void reorder(List<Integer> srcList, int srcPos,
+						int length, int[] dest, int destPos) {
+					for(int a = 0; a<length; a++) {
+						dest[destPos+a] = srcList.get(length-1-a+srcPos);
+					}
+				}
+			},
+			
+			/** "ABCDEF" is encoded as "BA"+"DC"+"FE" */
+			REVERSE_PAIRS() {
+				@Override
+				protected void reorder(List<Integer> srcList, int srcPos,
+						int length, int[] dest, int destPos) {
+					for(int a = 0; a<length; a+=2) {
+						if(a+1<length) {
+							dest[destPos+a] = srcList.get(srcPos+a+1);
+							dest[destPos+a+1] = srcList.get(srcPos+a);
+						} else {
+							dest[destPos+a] = srcList.get(srcPos+a);
+						}
+					}
+				}
+			},
+			
+			/** "ABCDEF" is encoded as "DEF"+"ABC" */
+			CUT_DECK() {
+				@Override
+				protected void reorder(List<Integer> srcList, int srcPos,
+						int length, int[] dest, int destPos) {
+					int split = length/2;
+					dest[destPos+length-1] = srcList.get(srcPos+length-1);
+					for(int a = 0; a<split; a++) {
+						dest[destPos+a] = srcList.get(srcPos+a+split);
+						dest[destPos+a+split] = srcList.get(srcPos+a);
+					}
+				}
+			},
+
+			/** "ABCDEF" is encoded as "CBA"+"FED" */
+			REVERSE_CUT_DECK() {
+				@Override
+				protected void reorder(List<Integer> srcList, int srcPos,
+						int length, int[] dest, int destPos) {
+					int split = length/2;
+					for(int a = 0; a<split; a++) {
+						dest[destPos+a] = srcList.get(srcPos+split-1-a);
+					}
+					for(int a = split; a<length; a++) {
+						dest[destPos+a] = srcList.get(srcPos+length-1-a+split);
+					}
+				}
+			};
+			
+			/** Reorder a series of integers.
+			 * 
+			 * @param srcList the integers to reorder
+			 * @param srcPos the index in the srcList to begin enumeration options
+			 * @param length the number of elements to transfer/reorder
+			 * @param dest the array to store the data in
+			 * @param destPos the first index in the dest array to write to
+			 */
+			protected abstract void reorder(List<Integer> srcList,int srcPos,int length,int[] dest,int destPos);
+		};
+		
+
+		private static ReorderType[] reorderTypes = new ReorderType[] { ReorderType.CUT_DECK, ReorderType.REVERSE_CUT_DECK, ReorderType.REVERSE, ReorderType.REVERSE_PAIRS };
+		private class Run {
+			RunType type = null;
+			int[] data = new int[100];
+			int length = 0;
+			
+			private void reset(RunType type) {
+				this.type = type;
+				length = 0;
+			}
+			
+			private int[] encode() {
+				ReorderType reorderType = reorderTypes[ (reorderCycle++)%reorderTypes.length ];
+				int arrayOffset;
+				int l;
+				if(RunType.BOTH_MARKERS.equals(type)) {
+					l = length-2;
+					arrayOffset = 1;
+				} else if (RunType.NO_MARKER.equals(type)) {
+					arrayOffset = 0;
+					l = length;
+				} else {
+					l = length-1;
+					arrayOffset = 1;
+				}
+				List<Integer> bytes = new ArrayList<>(length);
+				for(int a = 0; a<length; a++) {
+					bytes.add(data[a]);
+				}
+				reorderType.reorder(bytes, arrayOffset, l, data, arrayOffset);
+				if(substitutionModel!=null) {
+					substitutionModel.applySubstitutions(markerRule, data, arrayOffset, l);
+				}
+				return data;
+			}
+		}
+		
+		int capacity;
+		final Run currentRun = new Run();
+		final SubstitutionModel substitutionModel;
+		MarkerRule markerRule;
+		int reorderCycle = 0;
+
+		/** Create a Scrambler.
+		 * 
+		 * @param random the Random to use as data is being encoded. This may be null.
+		 * @param oneCount the number of 1's in a byte that are required to flag it as a marker.
+		 * @param substitutionModel the optional SubstitutionModel this object may
+		 * apply to replace bytes.
+		 */
+		public Layer(int capacitySeed,MarkerRule markerRule,SubstitutionModel substitutionModel) {
+			if(markerRule==null)
+				throw new NullPointerException();
+			this.markerRule = markerRule;
+			this.substitutionModel = substitutionModel;
+			resetRun();
+		}
+		
+		@Override
+		public synchronized void push(int b) throws IOException {
+			if(closed) throw new IllegalStateException("This Scrambler has already been closed.");
+			
+			boolean completesRun = false;
+			if(markerRule.isMarker(b)) {
+				if(RunType.NO_MARKER.equals(currentRun.type)) {
+					pushChunk(currentRun.encode(), currentRun.length);
+					resetRun();
+				}
+				
+				if(currentRun.type==null) {
+					currentRun.reset( RunType.STARTING_MARKER );
+				} else if(RunType.STARTING_MARKER.equals(currentRun.type)) {
+					currentRun.type = RunType.BOTH_MARKERS;
+					completesRun = true;
+				}
+			} else {
+				if(currentRun.type==null) {
+					currentRun.reset( RunType.NO_MARKER );
+				}
+			}
+			currentRun.data[currentRun.length++] = b;
+			if(completesRun || currentRun.length==capacity) {
+				pushChunk(currentRun.encode(), currentRun.length);
+				resetRun();
+			}
+		}
+		
+		private synchronized void pushChunk(int[] data, int length) throws IOException {
+			if(data.length==length) {
+				super.pushChunk(data);
+			} else {
+				int[] copy = new int[length];
+				System.arraycopy(data, 0, copy, 0, length);
+				super.pushChunk(copy);
+			}
+		}
+
+		private void resetRun() {
+			currentRun.reset(null);
+			capacity = (capacity + CAPACITY_INCR) % CAPACITY_MAX + 2;
+		}
+		
+		protected static int CAPACITY_MAX = 60;
+		protected static int CAPACITY_INCR = 11;
+		
+		protected void flush() throws IOException {
+			if(currentRun.type!=null) {
+				pushChunk(currentRun.encode(), currentRun.length);
+				resetRun();
+			}
+		}
+	}
+	
 	public interface SubstitutionModel {
 		
 		/** This alters bytes (ranging from [0,255]) so the data is transformed.
@@ -595,8 +796,8 @@ public class Scrambler {
 			this.rule = rule;
 			this.substitutionModel = substitutionModel;
 		}
-		public ScramblerLayer createLayer() {
-			return new ScramblerLayer(capacitySeed, rule, substitutionModel.clone());
+		public Layer createLayer() {
+			return new Layer(capacitySeed, rule, substitutionModel.clone());
 		}
 	}
 
@@ -641,7 +842,7 @@ public class Scrambler {
 
 		List<Integer> capacitySeeds = new ArrayList<>(256+32);
 		for(int a = 0; a<k.size(); a++) {
-			capacitySeeds.add(random.nextInt(ScramblerLayer.CAPACITY_MAX));
+			capacitySeeds.add(random.nextInt(Layer.CAPACITY_MAX));
 		}
 
 		char[] charArray = characterSet == null ? null : characterSet.toString().toCharArray();
@@ -666,7 +867,7 @@ public class Scrambler {
 	}
 	
 	protected ChainedByteEncoder createEncoder() {
-		ScramblerLayer[] copy = new ScramblerLayer[layers.size()];
+		Layer[] copy = new Layer[layers.size()];
 		for(int a = 0; a<layers.size(); a++) {
 			copy[a] = layers.get(a).createLayer();
 		}
