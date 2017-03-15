d5cc969a5de1e0b9f5aaa736ccb07b2be40f7d81
jeremy <jeremy.wood@mac.com>
2017-03-15 00:09:27 -0400

    Adding header & getSourcePaths() method

--- pump-jar/src/main/java/com/pump/breakout/WorkspaceContext.java
+++ pump-jar/src/main/java/com/pump/breakout/WorkspaceContext.java
@@ -1,3 +1,13 @@
+/**
+ * This software is released as part of the Pumpernickel project.
+ * 
+ * All com.pump resources in the Pumpernickel project are distributed under the
+ * MIT License:
+ * https://raw.githubusercontent.com/mickleness/pumpernickel/master/License.txt
+ * 
+ * More information about the Pumpernickel project is available here:
+ * https://mickleness.github.io/pumpernickel/
+ */
 package com.pump.breakout;
 
 import java.io.File;
@@ -91,5 +101,12 @@ public class WorkspaceContext {
 	 */
 	public Collection<String> getClassNames() {
 		return Collections.unmodifiableCollection(classNameToFileMap.keySet());
+	}
+
+	/**
+	 * Return the source paths this context uses.
+	 */
+	public Collection<File> getSourcePaths() {
+		return new HashSet<>(sourcePaths);
 	}
 }
