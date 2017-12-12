c41d839521917a43c25f6681da54e2b1696044b7
jeremy <jeremy.wood@mac.com>
2017-12-12 00:40:18 -0500

    Adding ZipIterator

---
+++ pump-data/src/main/java/com/pump/util/CloseableMeasurableIterator.java
@@ -0,0 +1,27 @@
+/**
+ * This software is released as part of the Pumpernickel project.
+ * 
+ * All com.pump resources in the Pumpernickel project are distributed under the
+ * MIT License:
+ * https://raw.githubusercontent.com/mickleness/pumpernickel/master/License.txt
+ * 
+ * More information about the Pumpernickel project is available here:
+ * https://mickleness.github.io/pumpernickel/
+ */
+package com.pump.util;
+
+import java.util.Iterator;
+
+/**
+ * This is an iterator that can be be set up in a try-with-resources block and
+ * has an optional {@link #getProgress()} method.
+ *
+ * @param <T>
+ */
+public interface CloseableMeasurableIterator<T> extends AutoCloseable, Iterator<T> {
+	
+	/**
+	 * Return the progress (as a fraction from [0,1]) of the amount read, or a negative number to indicate the progress is indeterminate.
+	 */
+	public float getProgress();
+}
