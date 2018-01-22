a6d2534c49dddfc4a8264f1f82c14b1300b0ef2c
jeremy <jeremy.wood@mac.com>
2018-01-22 00:48:16 -0500

    Resolving compiler errors

--- pump-release/src/main/java/com/pump/release/Workspace.java
+++ pump-release/src/main/java/com/pump/release/Workspace.java
@@ -18,7 +18,7 @@ import java.util.Map;
 import com.pump.blog.Blurb;
 import com.pump.data.BeanMap;
 import com.pump.data.Key;
-import com.pump.io.java.JavaClassSummary;
+import com.pump.io.parser.java.JavaClassSummary;
 import com.pump.release.Project.MissingPomException;
 
 public class Workspace {
@@ -26,59 +26,62 @@ public class Workspace {
 	static File DELETED_FILE = new File("deleted file");
 
 	static class BlurbInfo extends BeanMap {
-		
-		public static final Key<Class> KEY_CLASS = new Key<>(Class.class, "class");
-		public static final Key<Blurb> KEY_BLURB = new Key<>(Blurb.class, "blurb");
-		
-		public BlurbInfo(Class c,Blurb blurb) {
+
+		public static final Key<Class> KEY_CLASS = new Key<>(Class.class,
+				"class");
+		public static final Key<Blurb> KEY_BLURB = new Key<>(Blurb.class,
+				"blurb");
+
+		public BlurbInfo(Class c, Blurb blurb) {
 			KEY_CLASS.put(data, c);
 			KEY_BLURB.put(data, blurb);
 		}
-		
+
 		public Blurb getBlurb() {
 			return KEY_BLURB.get(getMap());
 		}
-		
+
 		public Class getBlurbClass() {
 			return KEY_CLASS.get(getMap());
 		}
 	}
 
-	File pumpernickelDir = new File(System.getProperty("user.dir")).getParentFile();
+	File pumpernickelDir = new File(System.getProperty("user.dir"))
+			.getParentFile();
 	Map<JarId, Project> projects = new HashMap<>();
 	File releaseDir;
 	Releases releases;
-	
+
 	public Workspace() {
 		releaseDir = new File(pumpernickelDir, "pump-release");
-		if(!releaseDir.exists())
+		if (!releaseDir.exists())
 			throw new RuntimeException("\"pump-release\" directory not found.");
 
 		releases = new Releases(releaseDir);
-		
-		for(File child : pumpernickelDir.listFiles()) {
-			if(child.isDirectory() && child.getName().startsWith("pump-")) {
+
+		for (File child : pumpernickelDir.listFiles()) {
+			if (child.isDirectory() && child.getName().startsWith("pump-")) {
 				try {
 					Project p = new Project(releases, child);
 					projects.put(p.getJarId(), p);
-				} catch(MissingPomException e) {
-					if(!child.equals(releaseDir)) {
+				} catch (MissingPomException e) {
+					if (!child.equals(releaseDir)) {
 						e.printStackTrace();
 					}
-				} catch(Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * Return the Releases object used to manage jar releases.
 	 */
 	public Releases getReleases() {
 		return releases;
 	}
-	
+
 	/**
 	 * Return a copy of all the available Projects keyed by their {@link JarId}.
 	 */
@@ -87,39 +90,40 @@ public class Workspace {
 	}
 
 	/**
-	 * Return the master "Pumpernickel" directory that contains all the subprojects.
+	 * Return the master "Pumpernickel" directory that contains all the
+	 * subprojects.
 	 */
 	public File getDirectory() {
 		return pumpernickelDir;
 	}
 
-	
-	/** Return the {@link BlurbInfo} associated with a java file, or null
-	 * if it cannot be identified.
+	/**
+	 * Return the {@link BlurbInfo} associated with a java file, or null if it
+	 * cannot be identified.
 	 */
 	public BlurbInfo getBlurb(File javaFile) {
 		try {
 			String className = JavaClassSummary.getClassName(javaFile);
 			return getBlurb(className);
-		} catch(IOException e) {
+		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
-	/** Return the {@link BlurbInfo} associated with a class name, or null
-	 * if it cannot be identified.
+	/**
+	 * Return the {@link BlurbInfo} associated with a class name, or null if it
+	 * cannot be identified.
 	 */
 	public static BlurbInfo getBlurb(String className) {
 		try {
 			Class<?> theClass = Class.forName(className);
 			Blurb blurb = theClass.getAnnotation(Blurb.class);
-			if(blurb==null)
+			if (blurb == null)
 				return null;
 			return new BlurbInfo(theClass, blurb);
-		} catch(ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
+
 }
