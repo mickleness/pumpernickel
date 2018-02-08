1c42f538b0e528869bde67d37b1bfc2675c17851
jeremy <jeremy.wood@mac.com>
2018-02-08 00:31:28 -0500

    Updating MovWriterDemo visual track
    Also fixing mistaken commit that made audio start at t=10s

--- pump-showcase/src/main/java/com/pump/showcase/MovWriterDemo.java
+++ pump-showcase/src/main/java/com/pump/showcase/MovWriterDemo.java
@@ -19,10 +19,13 @@
  */
 package com.pump.showcase;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -31,7 +34,10 @@ import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
@@ -40,6 +46,7 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.DefaultListCellRenderer;
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -55,12 +62,17 @@ import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import com.pump.animation.quicktime.JPEGMovWriter;
 import com.pump.animation.quicktime.MovWriter;
 import com.pump.animation.quicktime.PNGMovWriter;
 import com.pump.blog.Blurb;
+import com.pump.icon.FileIcon;
+import com.pump.swing.AnimationController;
 import com.pump.swing.FileDialogUtils;
+import com.pump.swing.PartialLineBorder;
 import com.pump.util.ObservableList;
 
 /**
@@ -81,6 +93,95 @@ import com.pump.util.ObservableList;
 public class MovWriterDemo extends JPanel {
 	private static final long serialVersionUID = 1L;
 
+	class MoviePainter {
+		public void paintFrame(Graphics g0, int w, int h, int frame) {
+			Graphics2D g = (Graphics2D) g0.create();
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+			Random random = new Random(0);
+			g.setColor(Color.white);
+			g.fillRect(0, 0, w, h);
+			Point2D center = new Point2D.Float(w / 2, h / 2);
+			double r = 100;
+			double theta = 0;
+			float hue1 = .5f;
+			float hueAnchor1 = .7f;
+			float hue2 = .75f;
+			float hueAnchor2 = .4f;
+			g.setColor(Color.gray);
+			float visibleFrameLimit = 20;
+
+			Point2D lastCursor = null;
+
+			Point2D cursor = new Point2D.Double(center.getX(), center.getY());
+			Point2D lastZ = null;
+			g.rotate(((double) frame) / 50.0, center.getX(), center.getY());
+			for (double a = 0; a < frame; a++) {
+				r += 20 * Math.cos(4 + 2 * Math.cos(.5 + a / 10.0));
+				r = Math.min(r, Math.min(w / 2, h / 2));
+				r = Math.max(20, r);
+				theta += Math.cos(2 + 2 * Math.sin(a / 2.0)) * .25;
+				hueAnchor1 += (float) ((random.nextDouble() - .5) * .2);
+				hueAnchor2 += (float) ((random.nextDouble() - .5) * .2);
+
+				Point2D anchor = new Point2D.Double(center.getX() + r
+						* Math.cos(theta), center.getY() + r * Math.sin(theta));
+				hue1 = .7f * hue1 + hueAnchor1 * .3f;
+				hue2 = .7f * hue2 + hueAnchor2 * .3f;
+
+				float opacity = (float) Math.max(0, a - (frame - 1)
+						+ visibleFrameLimit);
+				opacity = opacity / visibleFrameLimit;
+
+				cursor = new Point2D.Double(cursor.getX() * .8 + anchor.getX()
+						* .2, cursor.getY() * .8 + anchor.getY() * .2);
+
+				if (opacity > .001) {
+					g.setComposite(AlphaComposite.getInstance(
+							AlphaComposite.SRC_OVER, opacity));
+
+					if (lastCursor != null) {
+						for (int b = 0; b < 1000; b++) {
+							Point2D z = tween(cursor, lastCursor,
+									((float) b) / 1000f);
+							if (lastZ == null || lastZ.distance(z) > 2) {
+								paintSymmetryDot((Graphics2D) g.create(), z,
+										w / 2, h / 2, hue1, hue2);
+								lastZ = z;
+							}
+						}
+					} else {
+						paintSymmetryDot((Graphics2D) g.create(), cursor,
+								w / 2, h / 2, hue1, hue2);
+					}
+				}
+
+				lastCursor = cursor;
+			}
+			g.dispose();
+		}
+
+		private Point2D tween(Point2D p1, Point2D p2, float f) {
+			return new Point2D.Double(p1.getX() * f + p2.getX() * (1 - f),
+					p1.getY() * f + p2.getY() * (1 - f));
+		}
+
+		private void paintSymmetryDot(Graphics2D g, Point2D p, int x, int y,
+				float hue1, float hue2) {
+			for (int k = 0; k < 32; k++) {
+				if (k % 2 == 0)
+					g.setColor(Color.getHSBColor(hue1, .8f, .8f));
+				else
+					g.setColor(Color.getHSBColor(hue2, .8f, .8f));
+				g.rotate(1.0 / 32.0 * 2.0 * Math.PI, x, y);
+				float r = (k % 4 >= 2) ? 2 : 3;
+				g.fill(new Ellipse2D.Double(p.getX() - r, p.getY() - r, 2 * r,
+						2 * r));
+			}
+
+		}
+	}
+
 	JPanel visualPanel = new JPanel(new GridBagLayout());
 	JButton exportButton = new JButton("Export");
 	JSpinner framesSpinner = new JSpinner(new SpinnerNumberModel(100, 1, 50000,
@@ -94,10 +195,50 @@ public class MovWriterDemo extends JPanel {
 	ObservableList<File> audioFileList = new ObservableList<File>();
 	JList audioFiles = new JList(audioFileList.getListModelEDTMirror());
 	JButton addButton = new JButton("Add");
+	AnimationController animationController = new AnimationController();
+	JPanel animationPreview = new JPanel() {
+		MoviePainter p = new MoviePainter();
+
+		protected void paintComponent(Graphics g) {
+			float time = animationController.getTime();
+			int frame = (int) (time * ((Number) fpsSpinner.getValue())
+					.intValue());
+			p.paintFrame(g.create(), getWidth(), getHeight(), frame);
+
+			g.setColor(Color.black);
+			Font f = UIManager.getFont("Label.font");
+			g.setFont(f);
+			g.drawString("f = " + frame, 3, (int) (f.getSize2D() + 4));
+			g.drawString("t = " + time, 3, (int) (2 * f.getSize2D() + 8));
+		}
+	};
 
 	public MovWriterDemo() {
 		visualPanel.setBorder(new TitledBorder("Visual Track"));
 		audioPanel.setBorder(new TitledBorder("Audio Track"));
+
+		PropertyChangeListener repaintListener = new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				animationPreview.repaint();
+			}
+		};
+		animationController.addPropertyChangeListener(
+				AnimationController.TIME_PROPERTY, repaintListener);
+		animationController.addPropertyChangeListener(
+				AnimationController.PLAYING_PROPERTY, repaintListener);
+		animationController.addPropertyChangeListener(
+				AnimationController.DURATION_PROPERTY, repaintListener);
+
+		ChangeListener updateAnimationChangeListener = new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				float fps = ((Number) fpsSpinner.getValue()).intValue();
+				float frames = ((Number) framesSpinner.getValue()).intValue();
+				animationController.setDuration(frames / fps);
+			}
+		};
+		framesSpinner.addChangeListener(updateAnimationChangeListener);
 
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridx = 0;
@@ -135,13 +276,39 @@ public class MovWriterDemo extends JPanel {
 		c.gridy++;
 		audioPanel.add(addButton, c);
 
+		JPanel animationPreviewContainer = new JPanel(new GridBagLayout());
 		c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = 0;
 		c.weightx = 1;
 		c.weighty = 1;
-		c.anchor = GridBagConstraints.NORTH;
+		c.fill = GridBagConstraints.BOTH;
+		animationPreviewContainer.add(animationPreview, c);
+		c.gridy++;
+		c.weighty = 0;
+		animationPreviewContainer.add(animationController, c);
+		animationPreview.setBorder(new PartialLineBorder(Color.gray,
+				new Insets(1, 1, 0, 1)));
+		animationPreview.setPreferredSize(new Dimension(400, 300));
+
+		c = new GridBagConstraints();
 		setLayout(new GridBagLayout());
+		c.gridx = 0;
+		c.gridy = 0;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.fill = GridBagConstraints.BOTH;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.insets = new Insets(0, 5, 5, 5);
+		add(animationPreviewContainer, c);
+
+		c.fill = GridBagConstraints.NONE;
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 1;
+		c.weighty = 1;
+		c.anchor = GridBagConstraints.NORTH;
 
 		c.gridheight = GridBagConstraints.REMAINDER;
 		c.insets = new Insets(10, 10, 10, 10);
@@ -194,7 +361,10 @@ public class MovWriterDemo extends JPanel {
 					File file = (File) value;
 					JLabel label = (JLabel) c;
 					label.setText(file.getName());
-					label.setIcon(UIManager.getIcon("FileView.fileIcon"));
+					Icon icon = FileIcon.getIcon(file);
+					if (icon == null)
+						icon = UIManager.getIcon("FileView.fileIcon");
+					label.setIcon(icon);
 				}
 				return c;
 			}
@@ -294,11 +464,10 @@ public class MovWriterDemo extends JPanel {
 				File audioFile = audioFileList.get(a);
 				AudioInputStream audioIn = AudioSystem
 						.getAudioInputStream(audioFile);
-				anim.addAudioTrack(audioIn, 10);
+				anim.addAudioTrack(audioIn, 0);
 			}
 			float frameDuration = 1f / (fps);
 			progressBar.setMaximum(frameCount);
-			Random random = new Random(0);
 
 			for (; framesWritten < frameCount; framesWritten++) {
 				if (progressDialog.isVisible() == false
@@ -309,16 +478,10 @@ public class MovWriterDemo extends JPanel {
 				progressBar.setValue(framesWritten);
 				label.setText("Writing Frame " + (framesWritten + 1) + "...");
 
-				g = bi.createGraphics();
-				g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-						RenderingHints.VALUE_ANTIALIAS_ON);
-				Ellipse2D ellipse = new Ellipse2D.Float();
-				g.setColor(new Color(random.nextInt(255), random.nextInt(255),
-						random.nextInt(255)));
-				ellipse.setFrame(random.nextInt(600), random.nextInt(440), 40,
-						40);
-				g.fill(ellipse);
-				g.dispose();
+				MoviePainter p = new MoviePainter();
+				Graphics2D g2 = bi.createGraphics();
+				p.paintFrame(g2, bi.getWidth(), bi.getHeight(), framesWritten);
+				g2.dispose();
 
 				anim.addFrame(frameDuration, bi, null);
 			}
