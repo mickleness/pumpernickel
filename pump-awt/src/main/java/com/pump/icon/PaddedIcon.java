77be5ebb831e362332f979e136818d95bc71394c
jeremy <jeremy.wood@mac.com>
2018-04-22 01:49:41 -0400

    Adding a new constructor that accepts a Dimension

--- pump-awt/src/main/java/com/pump/icon/PaddedIcon.java
+++ pump-awt/src/main/java/com/pump/icon/PaddedIcon.java
@@ -11,8 +11,10 @@
 package com.pump.icon;
 
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.util.Objects;
 
 import javax.swing.Icon;
 
@@ -21,15 +23,54 @@ public class PaddedIcon implements Icon {
 	final Icon icon;
 	final Insets i;
 
+	/**
+	 * Create a PaddedIcon that fills extra space to fill the target dimensions
+	 * provided.
+	 * <p>
+	 * This throws an exception if the target size is smaller than the Icon
+	 * provided.
+	 * 
+	 * @param orig
+	 *            an optional Icon to pad.
+	 * @param targetSize
+	 *            the dimensions this PaddedIcon.
+	 */
+	public PaddedIcon(Icon orig, Dimension targetSize) {
+		Objects.requireNonNull(targetSize);
+
+		if (orig == null) {
+			icon = new EmptyIcon(targetSize.width, targetSize.height);
+			i = new Insets(0, 0, 0, 0);
+		} else {
+			icon = orig;
+			int x = targetSize.width - icon.getIconWidth();
+			int y = targetSize.height - icon.getIconHeight();
+
+			if (x < 0)
+				throw new IllegalArgumentException(
+						"Icon too wide to fit in target size. "
+								+ targetSize.width + "<" + icon.getIconWidth());
+			if (y < 0)
+				throw new IllegalArgumentException(
+						"Icon too tall to fit in target size. "
+								+ targetSize.height + "<"
+								+ icon.getIconHeight());
+
+			int top = y / 2;
+			int bottom = y - top;
+			int left = x / 2;
+			int right = x - left;
+			i = new Insets(top, left, bottom, right);
+		}
+	}
+
 	public PaddedIcon(Icon orig, int padding) {
 		this(orig, new Insets(padding, padding, padding, padding));
 	}
 
 	public PaddedIcon(Icon orig, Insets i) {
-		if (orig == null)
-			throw new NullPointerException();
-		if (i == null)
-			throw new NullPointerException();
+		Objects.requireNonNull(orig);
+		Objects.requireNonNull(i);
 		this.icon = orig;
 		this.i = i;
 	}
