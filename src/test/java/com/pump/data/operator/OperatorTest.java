49976af403be34cb74f450760231cc2519a4a7d8
jeremy <jeremy.wood@mac.com>
2019-08-03 16:19:45 -0400

    Removing unsupported LIKE merge (see previous commit notes)

--- src/test/java/com/pump/data/operator/OperatorTest.java
+++ src/test/java/com/pump/data/operator/OperatorTest.java
@@ -1112,13 +1112,5 @@ public class OperatorTest extends TestCase {
 		testEquals(false, "x != null", "matches(x, \"*A\")", format);
 		testEquals(false, "x != null", "matches(x, \"A*\")", format);
 		testEquals(false, "x != null", "matches(x, \"?\")", format);
-
-		Like startsWithJ = new Like("x", new WildcardPattern("J*"));
-		Like endsWithB = new Like("x", new WildcardPattern("*AB"));
-
-		Operator and = And.create(startsWithJ, endsWithB);
-		Like mergedLike = new Like("x", new WildcardPattern("J*AB"));
-
-		assertEquals(and, mergedLike);
 	}
 }
