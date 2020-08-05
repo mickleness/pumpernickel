e77979dc008b93f883d26c4e728d1ab2e282fdc9
Jeremy Wood <jeremy.wood@mac.com>
2020-08-05 05:19:36 -0400

    Migrate to JDK 9, use new Desktop features
    Previously I used some Apple-specific classes, which cause compiler
    errors on on-Macs. The features I need became standard in Java 9, so
    I'll try just migrating to that version.

--- src/main/java/com/pump/desktop/MacMoveToTrashOperation.java
+++ src/main/java/com/pump/desktop/MacMoveToTrashOperation.java
@@ -1,16 +1,18 @@
 package com.pump.desktop;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import com.apple.eio.FileManager;
 import com.pump.desktop.DesktopHelper.FileOperationType;
 
 class MacMoveToTrashOperation implements DesktopHelper.FileOperation {
 
 	@Override
 	public boolean execute(File file) throws FileNotFoundException {
-		return FileManager.moveToTrash(file);
+		if(Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.MOVE_TO_TRASH))
+			return Desktop.getDesktop().moveToTrash(file);
+		return false;
 	}
 
 	@Override
