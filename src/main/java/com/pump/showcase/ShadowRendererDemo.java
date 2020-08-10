a47bd6b3ef761fe5be37e5c084543ad6da2ebadf
jeremy <jeremy.wood@mac.com>
2020-08-10 01:58:51 -0400

    Making profiler cancellable, refactoring profiler

--- src/main/java/com/pump/showcase/ShadowRendererDemo.java
+++ src/main/java/com/pump/showcase/ShadowRendererDemo.java
@@ -3,6 +3,7 @@ package com.pump.showcase;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -26,6 +27,7 @@ import java.util.SortedMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -53,7 +55,11 @@ import com.pump.image.shadow.GaussianShadowRenderer;
 import com.pump.image.shadow.ShadowAttributes;
 import com.pump.image.shadow.ShadowRenderer;
 import com.pump.inspector.Inspector;
+import com.pump.showcase.Profiler.ProfileResults;
+import com.pump.showcase.ShadowRendererDemo.OriginalGaussianShadowRenderer;
 import com.pump.showcase.chart.LineChartRenderer;
+import com.pump.swing.DialogFooter;
+import com.pump.swing.DialogFooter.EscapeKeyBehavior;
 import com.pump.swing.JColorWell;
 import com.pump.swing.JFancyBox;
 import com.pump.swing.QDialog;
@@ -209,46 +215,6 @@ public class ShadowRendererDemo extends ShowcaseExampleDemo {
 
 	private static final long serialVersionUID = 1L;
 
-	static class ProfileResults {
-		Map<String, SortedMap<Double, Double>> results = new TreeMap<>();
-
-		public void store(ShadowRenderer renderer, float kernelSize,
-				long time) {
-			SortedMap<Double, Double> m = results
-					.get(renderer.getClass().getSimpleName());
-			if (m == null) {
-				m = new TreeMap<>();
-				results.put(renderer.getClass().getSimpleName(), m);
-			}
-			m.put((double) kernelSize, (double) time);
-		}
-
-		public void printTable() {
-			StringBuilder sb = new StringBuilder();
-			sb.append("Kernel\t");
-			for (String name : results.keySet()) {
-				sb.append(name);
-				sb.append("\t");
-			}
-			System.out.println(sb.toString().trim());
-
-			SortedSet<Double> allKeys = new TreeSet<>();
-			for (SortedMap<Double, Double> m : results.values()) {
-				allKeys.addAll(m.keySet());
-			}
-			for (Double key : allKeys) {
-				sb.delete(0, sb.length());
-				sb.append(key.toString());
-				sb.append("\t");
-				for (String name : results.keySet()) {
-					sb.append(results.get(name).get(key));
-					sb.append("\t");
-				}
-				System.out.println(sb.toString().trim());
-			}
-		}
-	}
-
 	/**
 	 * The maximum dx & dy offset for the shadow
 	 */
@@ -317,165 +283,32 @@ public class ShadowRendererDemo extends ShowcaseExampleDemo {
 		refreshExample();
 	}
 
-	static class RunSample implements Runnable {
-		ShadowRenderer renderer;
-		ShadowAttributes attr;
-		ARGBPixels srcPixels, dstPixels;
-		ProfileResults profileResults;
-
-		public RunSample(ProfileResults profileResults, ShadowRenderer renderer,
-				ShadowAttributes attr, ARGBPixels srcPixels,
-				ARGBPixels dstPixels) {
-			this.renderer = renderer;
-			this.attr = attr;
-			this.dstPixels = dstPixels;
-			this.srcPixels = srcPixels;
-			this.profileResults = profileResults;
-		}
-
-		public void run() {
-			long[] times = new long[6];
-			for (int a = 0; a < times.length; a++) {
-
-				times[a] = System.currentTimeMillis();
-				for (int b = 0; b < 100; b++) {
-					Arrays.fill(dstPixels.getPixels(), 0);
-					renderer.createShadow(srcPixels, dstPixels,
-							attr.getShadowKernelRadius(),
-							attr.getShadowColor());
-				}
-				times[a] = System.currentTimeMillis() - times[a];
-			}
-			Arrays.sort(times);
-			profileResults.store(renderer, attr.getShadowKernelRadius(),
-					times[times.length / 2]);
-		}
-	}
-
-	static class UpdateProgressBar implements Runnable {
-		JProgressBar progressBar;
-		int min, max, value;
-
-		UpdateProgressBar(JProgressBar progressBar, int min, int max,
-				int value) {
-			this.progressBar = progressBar;
-			this.min = min;
-			this.max = max;
-			this.value = value;
-		}
-
-		@Override
-		public void run() {
-			progressBar.getModel().setRangeProperties(value, 1, min, max,
-					false);
-			progressBar.setIndeterminate(false);
-		}
-	}
-
-	private void profileRenderers(ProfileResults profileResults,
-			final QDialog dialog, final JProgressBar progressBar,
-			Collection<ShadowRenderer> renderers) {
-		try {
-			ARGBPixels srcPixels = new ARGBPixels(srcImage);
-			srcImage.getRaster().getDataElements(0, 0, srcPixels.getWidth(),
-					srcPixels.getHeight(), srcPixels.getPixels());
-
-			List<Runnable> runnables = new LinkedList<>();
-			for (ShadowRenderer renderer : renderers) {
-				float min = 0;
-				float max = 25;
-				// load max first so we front more expensive things at the
-				// beginning of progress bar updates
-				for (float kernelSize = max; kernelSize >= min; kernelSize -= .5f) {
-					ShadowAttributes attr = new ShadowAttributes(0, 0,
-							kernelSize, Color.black);
-					int k = renderer.getKernel(attr.getShadowKernelRadius())
-							.getKernelRadius();
-					ARGBPixels dstPixels = new ARGBPixels(
-							srcImage.getWidth() + 2 * k,
-							srcImage.getHeight() + 2 * k);
-					runnables.add(new RunSample(profileResults, renderer, attr,
-							srcPixels, dstPixels));
-				}
-			}
-
-			SwingUtilities.invokeLater(
-					new UpdateProgressBar(progressBar, 0, runnables.size(), 0));
-			int ctr = 0;
-			int size = runnables.size();
-			while (!runnables.isEmpty()) {
-				Runnable runnable = runnables.remove(0);
-				runnable.run();
-				SwingUtilities.invokeLater(
-						new UpdateProgressBar(progressBar, 0, size, ctr++));
-			}
-
-			profileResults.printTable();
-		} catch (Throwable t) {
-			t.printStackTrace();
-		} finally {
-			SwingUtilities.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					dialog.setVisible(false);
-				}
-			});
-		}
-	}
-
 	@Override
 	protected JComponent getComponentBelowExamplePanel() {
 		JButton profileButton = new JButton("Profile Performance");
 
 		profileButton.addActionListener(new ActionListener() {
-			ProfileResults profileResults;
-			QDialog dialog;
-			JProgressBar progressBar;
+			Profiler profiler;
 
-			// frontload most expensive renderers first:
-			Collection<ShadowRenderer> renderers = Arrays.asList(
-					new OriginalGaussianShadowRenderer(),
-					new GaussianShadowRenderer(), new DoubleBoxShadowRenderer(),
-					new BoxShadowRenderer());
-
+			@Override
 			public void actionPerformed(ActionEvent e) {
-				progressBar = new JProgressBar(SwingConstants.HORIZONTAL, 0,
-						100);
-				progressBar.setIndeterminate(true);
 
 				JFrame frame = (JFrame) SwingUtilities
 						.getWindowAncestor(ShadowRendererDemo.this);
-				if (profileResults == null) {
-					profileResults = new ProfileResults();
-					JComponent content = QDialog.createContentPanel(
-							"Profiling ShadowRenderers...",
-							"Please wait while I test the execution time for different shadow configurations.",
-							progressBar, true);
-					dialog = new QDialog(frame, "Profiling",
-							QDialog.getIcon(QDialog.INFORMATION_MESSAGE),
-							content, null, false);
-					dialog.pack();
-					dialog.setLocationRelativeTo(frame);
-					Thread profileThread = new Thread() {
-						@Override
-						public void run() {
-							profileRenderers(profileResults, dialog,
-									progressBar, renderers);
-						}
-					};
-					profileThread.start();
-					dialog.setVisible(true);
+				if (profiler == null) {
+					profiler = new Profiler(ShadowRendererDemo.this);
 				}
 
-				try {
+				ProfileResults results = profiler.getResults();
+				if (results != null) {
 					LineChartRenderer renderer = new LineChartRenderer(
-							profileResults.results);
+							profiler.results.data);
 					BufferedImage bi = renderer.render(new Dimension(600, 400));
 					JLabel content = new JLabel(new ImageIcon(bi));
 					JFancyBox box = new JFancyBox(frame, content);
 					box.setVisible(true);
-				} catch (Exception e2) {
-					throw new RuntimeException(e2);
+				} else {
+					profiler = null;
 				}
 			}
 		});
@@ -568,4 +401,235 @@ public class ShadowRendererDemo extends ShowcaseExampleDemo {
 				BoxShadowRenderer.class, GaussianShadowRenderer.class };
 	}
 
+}
+
+/**
+ * This compares the performance of different ShadowRenderers as the kernel
+ * radius increases.
+ * <p>
+ * This class includes the UI and the comparison logic.
+ */
+class Profiler {
+
+	static class ProfileResults {
+		Map<String, SortedMap<Double, Double>> data = new TreeMap<>();
+
+		public void store(ShadowRenderer renderer, float kernelSize,
+				long time) {
+			SortedMap<Double, Double> m = data
+					.get(renderer.getClass().getSimpleName());
+			if (m == null) {
+				m = new TreeMap<>();
+				data.put(renderer.getClass().getSimpleName(), m);
+			}
+			m.put((double) kernelSize, (double) time);
+		}
+
+		public void printTable() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("Kernel\t");
+			for (String name : data.keySet()) {
+				sb.append(name);
+				sb.append("\t");
+			}
+			System.out.println(sb.toString().trim());
+
+			SortedSet<Double> allKeys = new TreeSet<>();
+			for (SortedMap<Double, Double> m : data.values()) {
+				allKeys.addAll(m.keySet());
+			}
+			for (Double key : allKeys) {
+				sb.delete(0, sb.length());
+				sb.append(key.toString());
+				sb.append("\t");
+				for (String name : data.keySet()) {
+					sb.append(data.get(name).get(key));
+					sb.append("\t");
+				}
+				System.out.println(sb.toString().trim());
+			}
+		}
+	}
+
+	static class UpdateProgressBar implements Runnable {
+		JProgressBar progressBar;
+		int min, max, value;
+
+		UpdateProgressBar(JProgressBar progressBar, int min, int max,
+				int value) {
+			this.progressBar = progressBar;
+			this.min = min;
+			this.max = max;
+			this.value = value;
+		}
+
+		@Override
+		public void run() {
+			progressBar.getModel().setRangeProperties(value, 1, min, max,
+					false);
+			progressBar.setIndeterminate(false);
+		}
+	}
+
+	static class RunSample implements Runnable {
+		ShadowRenderer renderer;
+		ShadowAttributes attr;
+		ARGBPixels srcPixels, dstPixels;
+		ProfileResults profileResults;
+
+		public RunSample(ProfileResults profileResults, ShadowRenderer renderer,
+				ShadowAttributes attr, ARGBPixels srcPixels,
+				ARGBPixels dstPixels) {
+			this.renderer = renderer;
+			this.attr = attr;
+			this.dstPixels = dstPixels;
+			this.srcPixels = srcPixels;
+			this.profileResults = profileResults;
+		}
+
+		public void run() {
+			long[] times = new long[6];
+			for (int a = 0; a < times.length; a++) {
+
+				times[a] = System.currentTimeMillis();
+				for (int b = 0; b < 100; b++) {
+					Arrays.fill(dstPixels.getPixels(), 0);
+					renderer.createShadow(srcPixels, dstPixels,
+							attr.getShadowKernelRadius(),
+							attr.getShadowColor());
+				}
+				times[a] = System.currentTimeMillis() - times[a];
+			}
+			Arrays.sort(times);
+			profileResults.store(renderer, attr.getShadowKernelRadius(),
+					times[times.length / 2]);
+		}
+	}
+
+	// frontload most expensive renderers first:
+	Collection<ShadowRenderer> renderers = Arrays.asList(
+			new OriginalGaussianShadowRenderer(), new GaussianShadowRenderer(),
+			new DoubleBoxShadowRenderer(), new BoxShadowRenderer());
+
+	ProfileResults results;
+	QDialog dialog;
+	JProgressBar progressBar;
+	ShadowRendererDemo demo;
+	Frame frame;
+
+	public Profiler(ShadowRendererDemo demo) {
+		this.demo = demo;
+		frame = (Frame) SwingUtilities.getWindowAncestor(demo);
+		progressBar = new JProgressBar(SwingConstants.HORIZONTAL, 0, 100);
+		progressBar.setIndeterminate(true);
+
+		JComponent content = QDialog.createContentPanel(
+				"Profiling ShadowRenderers...",
+				"Please wait while I test the execution time for different shadow configurations.",
+				progressBar, true);
+		DialogFooter footer = DialogFooter.createDialogFooter(
+				DialogFooter.CANCEL_OPTION, EscapeKeyBehavior.TRIGGERS_CANCEL);
+		dialog = new QDialog(frame, "Profiling",
+				QDialog.getIcon(QDialog.INFORMATION_MESSAGE), content, footer,
+				true);
+		footer.getButton(DialogFooter.CANCEL_OPTION)
+				.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						dialog.setVisible(false);
+					}
+				});
+	}
+
+	public ProfileResults getResults() {
+		if (results == null) {
+			results = createResults();
+		}
+		return results;
+	}
+
+	protected ProfileResults createResults() {
+		ProfileResults returnValue = new ProfileResults();
+		AtomicBoolean isCancelled = new AtomicBoolean(false);
+		AtomicBoolean isComplete = new AtomicBoolean(false);
+
+		dialog.pack();
+		dialog.setLocationRelativeTo(frame);
+
+		Thread profileThread = new Thread("ShadowRenderer-Profiler") {
+			@Override
+			public void run() {
+				profileRenderers(returnValue, dialog, progressBar, renderers,
+						isCancelled, isComplete);
+			}
+		};
+		profileThread.start();
+		dialog.setVisible(true);
+
+		if (!isComplete.get())
+			isCancelled.set(true);
+
+		if (isComplete.get())
+			return returnValue;
+		return null;
+	}
+
+	private void profileRenderers(ProfileResults profileResults,
+			final QDialog dialog, final JProgressBar progressBar,
+			Collection<ShadowRenderer> renderers, AtomicBoolean isCancelled,
+			AtomicBoolean isComplete) {
+		try {
+			ARGBPixels srcPixels = new ARGBPixels(demo.srcImage);
+			demo.srcImage.getRaster().getDataElements(0, 0,
+					srcPixels.getWidth(), srcPixels.getHeight(),
+					srcPixels.getPixels());
+
+			List<Runnable> runnables = new LinkedList<>();
+			for (ShadowRenderer renderer : renderers) {
+				float min = 0;
+				float max = 25;
+				// load max first so we front more expensive things at the
+				// beginning of progress bar updates
+				for (float kernelSize = max; kernelSize >= min; kernelSize -= .5f) {
+					ShadowAttributes attr = new ShadowAttributes(0, 0,
+							kernelSize, Color.black);
+					int k = renderer.getKernel(attr.getShadowKernelRadius())
+							.getKernelRadius();
+					ARGBPixels dstPixels = new ARGBPixels(
+							demo.srcImage.getWidth() + 2 * k,
+							demo.srcImage.getHeight() + 2 * k);
+					runnables.add(new RunSample(profileResults, renderer, attr,
+							srcPixels, dstPixels));
+				}
+			}
+
+			SwingUtilities.invokeLater(
+					new UpdateProgressBar(progressBar, 0, runnables.size(), 0));
+			int ctr = 0;
+			int size = runnables.size();
+			while (!runnables.isEmpty()) {
+				if (isCancelled.get()) {
+					break;
+				}
+				Runnable runnable = runnables.remove(0);
+				runnable.run();
+				SwingUtilities.invokeLater(
+						new UpdateProgressBar(progressBar, 0, size, ctr++));
+			}
+
+			isComplete.set(runnables.isEmpty());
+
+			if (isComplete.get())
+				profileResults.printTable();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		} finally {
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					dialog.setVisible(false);
+				}
+			});
+		}
+	}
 }
