d0936070d68cd4c77a8a6b604357177d40eb669e
jeremy <jeremy.wood@mac.com>
2021-05-09 04:32:36 -0400

    Trivial javadoc change

--- src/main/java/com/pump/plaf/button/MixedCheckBoxButtonGroup.java
+++ src/main/java/com/pump/plaf/button/MixedCheckBoxButtonGroup.java
@@ -17,56 +17,60 @@ import java.util.Objects;
 import javax.swing.JCheckBox;
 
 /**
- * This synchronized one master control button with several subordinate buttons. If all of the subordinate buttons are selected: the master button is selected. If none of the subordinate buttons are selected: the master button is unselected. Otherwise the master button renders in a mixed state to indicate that the group is only partially selected.
+ * This synchronizes one master control button with several subordinate buttons.
+ * If all of the subordinate buttons are selected: the master button is
+ * selected. If none of the subordinate buttons are selected: the master button
+ * is unselected. Otherwise the master button renders in a mixed state to
+ * indicate that the group is only partially selected.
  * <p>
- * If you interact with the master button you can toggle all subordinate buttons on/off.
+ * If you interact with the master button you can toggle all subordinate buttons
+ * on/off.
  */
 public class MixedCheckBoxButtonGroup {
 	JCheckBox masterButton;
 	JCheckBox[] subordinateButtons;
 	int adjusting = 0;
-	
+
 	ItemListener masterButtonListener = new ItemListener() {
 
 		@Override
 		public void itemStateChanged(ItemEvent e) {
-			if(adjusting>0)
+			if (adjusting > 0)
 				return;
 			adjusting++;
 			try {
 				MixedCheckBoxState.setMixed(masterButton, false);
-				for(JCheckBox subordinateButton : subordinateButtons) {
+				for (JCheckBox subordinateButton : subordinateButtons) {
 					subordinateButton.setSelected(masterButton.isSelected());
 				}
 			} finally {
 				adjusting--;
 			}
 		}
-		
+
 	};
 
-	
 	ItemListener subordinateButtonListener = new ItemListener() {
 
 		@Override
 		public void itemStateChanged(ItemEvent e) {
-			if(adjusting>0)
+			if (adjusting > 0)
 				return;
 			adjusting++;
 			try {
 				boolean anySelected = false;
 				boolean anyUnselected = false;
-				for(JCheckBox b : subordinateButtons) {
-					if(b.isVisible() && b.isSelected())
+				for (JCheckBox b : subordinateButtons) {
+					if (b.isVisible() && b.isSelected())
 						anySelected = true;
-					if(b.isVisible() && !b.isSelected())
+					if (b.isVisible() && !b.isSelected())
 						anyUnselected = true;
 				}
-				
-				if(!anyUnselected) {
+
+				if (!anyUnselected) {
 					MixedCheckBoxState.setMixed(masterButton, false);
 					masterButton.setSelected(true);
-				} else if(anySelected) {
+				} else if (anySelected) {
 					MixedCheckBoxState.setMixed(masterButton, true);
 					masterButton.setSelected(false);
 				} else {
@@ -77,18 +81,20 @@ public class MixedCheckBoxButtonGroup {
 				adjusting--;
 			}
 		}
-		
+
 	};
-	
-	public MixedCheckBoxButtonGroup(JCheckBox masterButton, JCheckBox... subordinateButtons) {
+
+	public MixedCheckBoxButtonGroup(JCheckBox masterButton,
+			JCheckBox... subordinateButtons) {
 		Objects.requireNonNull(masterButton);
 		Objects.requireNonNull(subordinateButtons);
 		this.masterButton = masterButton;
 		this.subordinateButtons = subordinateButtons;
 		masterButton.getModel().addItemListener(masterButtonListener);
-		
-		for(JCheckBox subordinateButton : subordinateButtons) {
-			subordinateButton.getModel().addItemListener(subordinateButtonListener);
+
+		for (JCheckBox subordinateButton : subordinateButtons) {
+			subordinateButton.getModel()
+					.addItemListener(subordinateButtonListener);
 		}
 
 		subordinateButtonListener.itemStateChanged(null);
