b822c4e8114326f0c3492f39d30857ab34e3d256
jeremy <jeremy.wood@mac.com>
2022-06-05 12:11:06 -0400

    Make MutableBufferedImage serializable
    One notable downside here is I have to add a useless zero-argument
    constructor.

--- src/main/java/com/pump/image/MutableBufferedImage.java
+++ src/main/java/com/pump/image/MutableBufferedImage.java
@@ -10,11 +10,16 @@
  */
 package com.pump.image;
 
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.ImageObserver;
 import java.awt.image.IndexColorModel;
 import java.awt.image.WritableRaster;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -24,9 +29,11 @@ import java.util.Map;
 
 /**
  * This is a <code>BufferedImage</code> that offers a <code>setProperty()</code>
- * method.
+ * method, is serializable, and supports
+ * {@link #equals(Object)}/{@link #hashCode()}.
  */
-public class MutableBufferedImage extends BufferedImage {
+public class MutableBufferedImage extends BufferedImage
+		implements Externalizable {
 
 	/**
 	 * Return a map of all known properties of a BufferedImage.
@@ -56,6 +63,16 @@ public class MutableBufferedImage extends BufferedImage {
 
 	Map<String, Object> extraProperties = null;
 
+	/**
+	 * This constructor should not be used. It is only available for
+	 * deserialization.
+	 * 
+	 * @deprecated use any other constructor
+	 */
+	public MutableBufferedImage() {
+		super(1, 1, BufferedImage.TYPE_INT_RGB);
+	}
+
 	public MutableBufferedImage(ColorModel cm, WritableRaster r,
 			boolean premultiplied, Hashtable<String, Object> properties) {
 		super(cm, r, premultiplied, properties);
@@ -78,6 +95,7 @@ public class MutableBufferedImage extends BufferedImage {
 	public MutableBufferedImage(BufferedImage bi) {
 		this(bi.getColorModel(), bi.getRaster(), bi.isAlphaPremultiplied(),
 				getPropertiesHashtable(bi));
+		setAccelerationPriority(bi.getAccelerationPriority());
 	}
 
 	@Override
@@ -125,5 +143,171 @@ public class MutableBufferedImage extends BufferedImage {
 		if (extraProperties == null)
 			extraProperties = new HashMap<String, Object>();
 		extraProperties.putAll(properties);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof BufferedImage))
+			return false;
+
+		int w = getWidth();
+		int h = getHeight();
+
+		BufferedImage other = (BufferedImage) o;
+		if (w != other.getWidth() || h != other.getHeight()
+				|| getType() != other.getType())
+			return false;
+		if (!getColorModel().equals(other.getColorModel()))
+			return false;
+		if (!getRaster().getClass().equals(other.getRaster().getClass()))
+			return false;
+
+		Object row1 = null;
+		Object row2 = null;
+		for (int y = 0; y < h; y++) {
+			row1 = getRaster().getDataElements(0, y, w, 1, row1);
+			row2 = other.getRaster().getDataElements(0, y, w, 1, row2);
+
+			if (row1 instanceof int[]) {
+				if (!Arrays.equals((int[]) row1, (int[]) row2))
+					return false;
+			} else if (row1 instanceof byte[]) {
+				if (!Arrays.equals((byte[]) row1, (byte[]) row2))
+					return false;
+			} else if (row1 instanceof short[]) {
+				if (!Arrays.equals((short[]) row1, (short[]) row2))
+					return false;
+			} else {
+				// I'm not sure if this can ever happen? If it does we can scale
+				// this method up
+				throw new IllegalStateException(row1.getClass().getName());
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int w = getWidth();
+		int h = getHeight();
+		int returnValue = (getWidth() << 14) + (getHeight() << 4) + getType();
+
+		for (Point p : new Point[] { new Point(0, 0), new Point(w - 1, 0),
+				new Point(0, h - 1), new Point(w - 1, h - 1),
+				new Point(w / 2, h / 2) }) {
+			int argb = getRGB(p.x, p.y);
+			returnValue ^= argb;
+		}
+
+		return returnValue;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + "["
+				+ Integer.toHexString(hashCode()) + ", type = " + getType()
+				+ ", colorModel = " + getColorModel() + ", raster = "
+				+ getRaster() + ", properties = " + getProperties(this) + "]";
+	}
+
+	///////// serialization:
+
+	/**
+	 * Return true if this object can be serialized, or false if attempting to
+	 * serialize this image will throw an IOException.
+	 * <p>
+	 * For example: if {@link #getType()} returns
+	 * {@link BufferedImage#TYPE_CUSTOM} then this method will return false.
+	 */
+	public boolean isSerializationSupported() {
+		return getSerializationUnsupportedReason() != null;
+	}
+
+	/**
+	 * Return a String for an IOException explaining why this image shouldn't be
+	 * serialized, or null if this image is serializable.
+	 */
+	private String getSerializationUnsupportedReason() {
+		switch (getType()) {
+		case BufferedImage.TYPE_3BYTE_BGR:
+		case BufferedImage.TYPE_4BYTE_ABGR:
+		case BufferedImage.TYPE_4BYTE_ABGR_PRE:
+		case BufferedImage.TYPE_BYTE_GRAY:
+		case BufferedImage.TYPE_INT_ARGB:
+		case BufferedImage.TYPE_INT_ARGB_PRE:
+		case BufferedImage.TYPE_INT_BGR:
+		case BufferedImage.TYPE_INT_RGB:
+			return null;
+
+		case BufferedImage.TYPE_BYTE_BINARY:
+		case BufferedImage.TYPE_BYTE_INDEXED:
+			// IndexColorModels COULD be supported, I just haven't gotten around
+			// to it:
+			return "This MutableBufferedImage is not serializable. IndexColorModels are not supported.";
+
+		case BufferedImage.TYPE_CUSTOM:
+		default:
+			return "This MutableBufferedImage is not serializable. This image type ("
+					+ getType() + ") is not supported.";
+
+		}
+	}
+
+	private transient MutableBufferedImage replacementImage;
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		String msg = getSerializationUnsupportedReason();
+		if (msg != null)
+			throw new IOException(msg);
+
+		out.writeInt(0);
+		out.writeInt(getWidth());
+		out.writeInt(getHeight());
+		out.writeInt(getType());
+		out.writeFloat(getAccelerationPriority());
+		out.writeObject(getProperties(this));
+		out.writeObject(getRaster().getDataElements(0, 0, getWidth(),
+				getHeight(), null));
+	}
+
+	@Override
+	public void readExternal(ObjectInput in)
+			throws IOException, ClassNotFoundException {
+		int internalVersion = in.readInt();
+		if (internalVersion == 0) {
+			int width = in.readInt();
+			int height = in.readInt();
+			int type = in.readInt();
+			float accelerationPriority = in.readFloat();
+			Map<String, Object> properties = (Map<String, Object>) in
+					.readObject();
+			Object rasterData = in.readObject();
+
+			// This approach is a little bit wasteful, because we have a large
+			// primitive array and now we're about to create an array of the
+			// same size and populate it.
+
+			// If this waste comes up on profilers we can save some short-term
+			// memory usage and probably get a little speed increase if create
+			// our own DataBuffer from the array and create our own
+			// WritableRaster. But for now I don't plan on using this in a hot
+			// loop so I'm not sure that optimization is necessary.
+
+			replacementImage = new MutableBufferedImage(width, height, type);
+			replacementImage.setAccelerationPriority(accelerationPriority);
+			replacementImage.getRaster().setDataElements(0, 0, width, height,
+					rasterData);
+			replacementImage.setProperties(properties);
+		}
+	}
+
+	private Object readResolve() {
+		if (replacementImage != null) {
+			return replacementImage;
+		}
+		return this;
 	}
 }
---
+++ src/test/java/com/pump/image/MutableBufferedImageTest.java
@@ -0,0 +1,85 @@
+package com.pump.image;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Random;
+
+import org.junit.Test;
+
+public class MutableBufferedImageTest {
+
+	@Test
+	public void testEquals() {
+		MutableBufferedImage bi1 = createSampleImage(60, 40,
+				BufferedImage.TYPE_INT_ARGB);
+		MutableBufferedImage bi2 = createSampleImage(60, 40,
+				BufferedImage.TYPE_INT_ARGB);
+		assertEquals(bi1, bi2);
+		assertEquals(bi1.hashCode(), bi2.hashCode());
+
+		// change the image type:
+		bi2 = createSampleImage(60, 40, BufferedImage.TYPE_INT_RGB);
+		assertFalse(bi1.equals(bi2));
+
+		// change pixels
+		for (int y = 0; y < bi1.getHeight(); y++) {
+			for (int x = 0; x < bi1.getWidth(); x++) {
+				bi2 = createSampleImage(60, 40, BufferedImage.TYPE_INT_ARGB);
+				int argb = bi2.getRGB(x, y);
+				argb++;
+				bi2.setRGB(x, y, argb);
+				assertFalse(bi1.equals(bi2));
+			}
+		}
+	}
+
+	@Test
+	public void testSerialization() throws Exception {
+		MutableBufferedImage bi = createSampleImage(60, 40,
+				BufferedImage.TYPE_INT_ARGB);
+		byte[] bytes = serialize(bi);
+		MutableBufferedImage bi2 = (MutableBufferedImage) deserialize(bytes);
+		assertEquals(bi, bi2);
+	}
+
+	private byte[] serialize(Serializable obj) throws IOException {
+		try (ByteArrayOutputStream byteOut = new ByteArrayOutputStream()) {
+			try (ObjectOutputStream objOut = new ObjectOutputStream(byteOut)) {
+				objOut.writeObject(obj);
+			}
+			byteOut.flush();
+			return byteOut.toByteArray();
+		}
+	}
+
+	private Serializable deserialize(byte[] bytes) throws Exception {
+		try (ByteArrayInputStream byteIn = new ByteArrayInputStream(bytes)) {
+			try (ObjectInputStream objIn = new ObjectInputStream(byteIn)) {
+				return (Serializable) objIn.readObject();
+			}
+		}
+	}
+
+	private MutableBufferedImage createSampleImage(int width, int height,
+			int type) {
+		MutableBufferedImage returnValue = new MutableBufferedImage(width,
+				height, type);
+		Random r = new Random(0);
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int argb = 0xff000000 + r.nextInt(0xffffff);
+				returnValue.setRGB(x, y, argb);
+			}
+		}
+		return returnValue;
+	}
+
+}
