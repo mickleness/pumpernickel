f9fc2ac0ecd827f474cc8743aa6ddecdd9762314
jeremy <jeremy.wood@mac.com>
2023-02-06 02:27:31 -0500

    Adding PointIterator
    I forget what motivated this class; I'm just cleaning up old unstaged
    changes today.

---
+++ src/main/java/com/pump/util/PointIterator.java
@@ -0,0 +1,97 @@
+package com.pump.util;
+
+import java.awt.Point;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * This creates a "square spiral" of points that traverse right, down, left, up,
+ * etc. This guarantees increasing coverage radiating out from an initial point,
+ * but because it is boxy (and not circular) the distances of the incoming
+ * points are not always in increasing order.
+ */
+public class PointIterator implements Iterator<Point> {
+
+	/**
+	 * This identifies the point the PointIterator will return for a given
+	 * iteration.
+	 * <p>
+	 * That is: in this example p1 and p2 are always equal: <code>
+	 * PointIterator iter = new PointIterator(0, 0, 1_000_000);
+	 * int ctr = 0;
+	 * while (iter.hasNext()) {
+	 *   Point p1 = iter.next();
+	 *   Point p2 = PointIterator.getPoint(ctr);
+	 *   ctr++;
+	 * }
+	 * </code>
+	 */
+	public static Point getPoint(int iteration) {
+		double sqrt = Math.sqrt(iteration);
+		int q = (int) Math.floor(sqrt);
+
+		int j = iteration - q * q;
+
+		if (q % 2 == 1) {
+			if (j <= q)
+				return new Point(1 + (q - 1) / 2, -(q - 1) / 2 + j);
+
+			return new Point(1 + (q - 1) / 2 - (j - q), (q - 1) / 2 + 1);
+		} else {
+			if (j <= q)
+				return new Point(-q / 2, q / 2 - j);
+			return new Point(-q / 2 + (j - q), -q / 2);
+		}
+	}
+
+	private static final int RIGHT = 0;
+	private static final int DOWN = 1;
+	private static final int LEFT = 2;
+	private static final int UP = 3;
+
+	private int iterations;
+	private int x, y;
+
+	int dir = UP;
+	int stepCtr = 0;
+	int stepMax = 0;
+
+	public PointIterator(int x, int y, int iterations) {
+		this.x = x;
+		this.y = y + 1;
+		this.iterations = iterations;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return iterations > 0;
+	}
+
+	@Override
+	public Point next() {
+		iterations--;
+		if (iterations < 0)
+			throw new NoSuchElementException();
+
+		if (dir == RIGHT) {
+			x++;
+		} else if (dir == DOWN) {
+			y++;
+		} else if (dir == LEFT) {
+			x--;
+		} else if (dir == UP) {
+			y--;
+		}
+
+		stepCtr++;
+		if (stepCtr >= stepMax) {
+			stepCtr = 0;
+			dir = (dir + 1) % 4;
+			if (dir % 2 == 0)
+				stepMax++;
+		}
+
+		return new Point(x, y);
+	}
+
+}
---
+++ src/test/java/com/pump/util/PointIteratorTest.java
@@ -0,0 +1,89 @@
+package com.pump.util;
+
+import java.awt.Point;
+import java.util.NoSuchElementException;
+
+import junit.framework.TestCase;
+
+public class PointIteratorTest extends TestCase {
+
+	Point[] expectedPoints = new Point[] { new Point(0, 0), new Point(1, 0),
+			new Point(1, 1), new Point(0, 1), new Point(-1, 1),
+			new Point(-1, 0), new Point(-1, -1), new Point(0, -1),
+			new Point(1, -1), new Point(2, -1), new Point(2, 0),
+			new Point(2, 1), new Point(2, 2), new Point(1, 2), new Point(0, 2),
+			new Point(-1, 2), new Point(-2, 2), new Point(-2, 1),
+			new Point(-2, 0), new Point(-2, -1), new Point(-2, -2),
+			new Point(-1, -2), new Point(0, -2), new Point(1, -2),
+			new Point(2, -2), new Point(3, -2), new Point(3, -1),
+			new Point(3, 0), new Point(3, 1), new Point(3, 2), new Point(3, 3),
+			new Point(2, 3), new Point(1, 3), new Point(0, 3), new Point(-1, 3),
+			new Point(-2, 3), new Point(-3, 3) };
+
+	public void testIterator_origin() {
+		PointIterator iter = new PointIterator(0, 0, expectedPoints.length);
+
+		for (Point expectedPoint : expectedPoints) {
+			assertTrue(iter.hasNext());
+			assertEquals(expectedPoint, iter.next());
+		}
+		assertFalse(iter.hasNext());
+
+		try {
+			iter.next();
+			fail();
+		} catch (NoSuchElementException e) {
+			// pass
+		}
+
+	}
+
+	public void testIterator_offset() {
+		int cx = 131;
+		int cy = -99;
+		PointIterator iter = new PointIterator(cx, cy, expectedPoints.length);
+
+		for (Point expectedPoint : expectedPoints) {
+			assertTrue(iter.hasNext());
+			assertEquals(new Point(expectedPoint.x + cx, expectedPoint.y + cy),
+					iter.next());
+		}
+		assertFalse(iter.hasNext());
+
+		try {
+			iter.next();
+			fail();
+		} catch (NoSuchElementException e) {
+			// pass
+		}
+	}
+
+	public void testGetPoint() {
+		// one round of the spiral (based on a paper chart):
+		assertEquals(new Point(2, -1), PointIterator.getPoint(9));
+		assertEquals(new Point(2, 0), PointIterator.getPoint(10));
+		assertEquals(new Point(2, 1), PointIterator.getPoint(11));
+		assertEquals(new Point(2, 2), PointIterator.getPoint(12));
+		assertEquals(new Point(1, 2), PointIterator.getPoint(13));
+		assertEquals(new Point(0, 2), PointIterator.getPoint(14));
+		assertEquals(new Point(-1, 2), PointIterator.getPoint(15));
+		assertEquals(new Point(-2, 2), PointIterator.getPoint(16));
+		assertEquals(new Point(-2, 1), PointIterator.getPoint(17));
+		assertEquals(new Point(-2, 0), PointIterator.getPoint(18));
+		assertEquals(new Point(-2, -1), PointIterator.getPoint(19));
+		assertEquals(new Point(-2, -2), PointIterator.getPoint(20));
+		assertEquals(new Point(-1, -2), PointIterator.getPoint(21));
+		assertEquals(new Point(0, -2), PointIterator.getPoint(22));
+		assertEquals(new Point(1, -2), PointIterator.getPoint(23));
+		assertEquals(new Point(2, -2), PointIterator.getPoint(24));
+		assertEquals(new Point(3, -2), PointIterator.getPoint(25));
+
+		PointIterator iter = new PointIterator(0, 0, 1_000_000);
+		int ctr = 0;
+		while (iter.hasNext()) {
+			Point p = iter.next();
+			assertEquals(p, PointIterator.getPoint(ctr));
+			ctr++;
+		}
+	}
+}
