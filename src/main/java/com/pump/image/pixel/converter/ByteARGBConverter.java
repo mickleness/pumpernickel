fa13070b50d2e8e5bcf884e11bf3e22f35f6c8ac
jeremy <jeremy.wood@mac.com>
2023-05-07 03:35:00 -0400

    Updating source code headers

--- src/main/java/com/pump/image/pixel/converter/ByteARGBConverter.java
+++ src/main/java/com/pump/image/pixel/converter/ByteARGBConverter.java
@@ -1,3 +1,13 @@
+/**
+ * This software is released as part of the Pumpernickel project.
+ * 
+ * All com.pump resources in the Pumpernickel project are distributed under the
+ * MIT License:
+ * https://github.com/mickleness/pumpernickel/raw/master/License.txt
+ * 
+ * More information about the Pumpernickel project is available here:
+ * https://mickleness.github.io/pumpernickel/
+ */
 package com.pump.image.pixel.converter;
 
 public class ByteARGBConverter implements PixelConverter<byte[]> {
