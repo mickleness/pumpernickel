2547a4d6204060b0bc3855e2925075967ab954b7
jeremy <jeremy.wood@mac.com>
2023-06-23 00:53:18 -0400

    Transitioning new builds to 1.04-beta

--- src/main/java/com/pump/showcase/app/PumpernickelShowcaseApp.java
+++ src/main/java/com/pump/showcase/app/PumpernickelShowcaseApp.java
@@ -98,7 +98,7 @@ public class PumpernickelShowcaseApp extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	public static final String VERSION = "1.03";
+	public static final String VERSION = "1.04-beta";
 
 	public static void main(String[] args) throws Exception {
 		DesktopApplication app = new DesktopApplication("com.pump.showcase",
