212626533a811eb27a52795e0bc80ee2c47e30af
jeremy <jeremy.wood@mac.com>
2023-07-11 16:46:17 -0400

    Cleaning up JPEGThumbnailImageReader

--- src/main/java/com/pump/image/jpeg/JPEGThumbnailImageReaderTest.java
+++ src/main/java/com/pump/image/jpeg/JPEGThumbnailImageReaderTest.java
@@ -1,6 +1,7 @@
 package com.pump.image.jpeg;
 
-import java.util.List;
+import java.util.*;
+
 import com.pump.UserCancelledException;
 import junit.framework.TestCase;
 
@@ -9,6 +10,7 @@ import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
 import javax.imageio.ImageTypeSpecifier;
 import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.spi.IIORegistry;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.stream.ImageInputStream;
 import java.awt.*;
@@ -16,9 +18,7 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Iterator;
-import java.util.Locale;
-import java.util.Objects;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class JPEGThumbnailImageReaderTest extends TestCase {
@@ -40,15 +40,21 @@ public class JPEGThumbnailImageReaderTest extends TestCase {
     }
 
     public void testImageIOFails() {
+        IIORegistry.getDefaultInstance().registerServiceProvider(new JPEGThumbnailImageReaderSpi());
+
         URL jpegFile = UserCancelledException.class.getResource("showcase/resourcegenerator/IMG-20171107-WA0002.jpg");
         Iterator iterator = ImageIO.getImageReadersBySuffix("jpeg");
         while(iterator.hasNext()) {
             ImageReader reader = (ImageReader)iterator.next();
+
+            if (reader instanceof JPEGThumbnailImageReader)
+                continue;
+
             try {
                 reader.setInput( ImageIO.createImageInputStream(jpegFile.openStream()) );
                 BufferedImage thumbnail = reader.readThumbnail(0, 0);
                 if (thumbnail != null) {
-                    fail("ImageIO DID have ImageReaders capable of reading the JPEG thumbnail. This means the pump classes may be obsolete.");
+                    fail("ImageIO had ImageReaders capable of reading the JPEG thumbnail. This means the pump classes may be obsolete.");
                 }
             } catch(Exception e) {
                 // we expect this to fail
@@ -61,6 +67,15 @@ class JPEGThumbnailImageReaderSpi extends ImageReaderSpi {
 
     public JPEGThumbnailImageReaderSpi() {
         inputTypes = new Class<?>[] { ImageInputStream.class };
+        pluginClassName = JPEGThumbnailImageReader.class.getName();
+
+        vendorName = "Pumpernickel";
+        version = "1.0";
+
+        // these are copied from the JPEGImageReaderSpi
+        suffixes = new String[] {"jpg", "jpeg"};
+        MIMETypes = new String[] {"image/jpeg"};
+        names = new String[] {"JPEG", "jpeg", "JPG", "jpg"};
     }
 
     @Override
@@ -81,76 +96,179 @@ class JPEGThumbnailImageReaderSpi extends ImageReaderSpi {
 
 class JPEGThumbnailImageReader extends ImageReader {
 
-    List<Dimension> thumbnailSizes = null;
+    boolean isInitialized = false;
+    List<Dimension> thumbnailDimensions = new ArrayList<>();
     List<BufferedImage> thumbnails = null;
+    Dimension imageSize = new Dimension(0,0);
 
     protected JPEGThumbnailImageReader(ImageReaderSpi originatingProvider) {
         super(originatingProvider);
     }
 
     @Override
+    public void reset() {
+        _reset();
+        super.reset();
+    }
+
+    @Override
+    public void setInput(Object input,
+                                      boolean seekForwardOnly,
+                                      boolean ignoreMetadata) {
+        _reset();
+        super.setInput(input, seekForwardOnly, ignoreMetadata);
+    }
+
+    private synchronized void _reset() {
+        thumbnails = null;
+        isInitialized = false;
+        thumbnailDimensions.clear();
+    }
+
+    @Override
+    public void dispose() {
+        _reset();
+        super.dispose();
+    }
+
+    /**
+     * This does two things:
+     * A. it scans the JPG image and catalogs all the possible thumbnail dimensions it offers.
+     * B. If you request a specific thumbnail index, it returns that thumbnail as a BufferedImage.
+     *
+     * If the ImageInputStream can only be read once, then this method stores a reference to all
+     * possible thumbnails.
+     *
+     * @param requestedThumbnailIndex the thumbnail index, or -1 if no specific thumbnail is needed
+     * @return the specific thumbnail requested, or null if no thumbnail was requested.
+     */
+    private synchronized BufferedImage initialize(final int requestedThumbnailIndex) throws IOException {
+        if (isInitialized) {
+            // we've already initialized once
+
+            if (requestedThumbnailIndex == -1)
+                return null;
+
+            // here the caller expects a specific thumbnail
+
+            if (thumbnails != null)
+                return thumbnails.get(requestedThumbnailIndex);
+
+            // if we're here: that means we've cataloged the basic thumbnail sizes already, but we didn't keep a reference
+            // to the BufferedImages themselves. So we'll need to scan again.
+        }
+
+        isInitialized = true;
+
+        AtomicReference<BufferedImage> returnValue = new AtomicReference<>();
+
+        thumbnails = isSeekForwardOnly() ? new ArrayList<>() : null;
+        thumbnailDimensions.clear();
+
+        ImageInputStream iis = (ImageInputStream) input;
+        iis.reset();
+
+        JPEGMetaDataListener listener = new JPEGMetaDataListener() {
+
+            @Override
+            public boolean isThumbnailAccepted(String markerName, int width, int height) {
+                thumbnailDimensions.add(new Dimension(width, height));
+
+                if (thumbnails != null)
+                    return true;
+
+                return thumbnailDimensions.size() == requestedThumbnailIndex + 1;
+            }
+
+            @Override
+            public void addProperty(String markerName, String propertyName, Object value) {
+                // intentionally empty
+            }
+
+            @Override
+            public void addThumbnail(String markerName, BufferedImage bi) {
+                if (thumbnailDimensions.size() == requestedThumbnailIndex + 1) {
+                    returnValue.set(bi);
+                }
+                if (thumbnails != null)
+                   thumbnails.add(bi);
+            }
+
+            @Override
+            public void addComment(String markerName, String comment) {
+                // intentionally empty
+            }
+
+            @Override
+            public void startFile() {
+
+            }
+
+            @Override
+            public void endFile() {
+
+            }
+
+            @Override
+            public void imageDescription(int bitsPerPixel, int width, int height, int numberOfComponents) {
+                imageSize.setSize(width, height);
+            }
+
+            @Override
+            public void processException(Exception e, String markerCode) {
+                // intentionally empty
+            }
+        };
+        JPEGMetaData.read(new InputStreamConverter(iis), listener);
+
+        return returnValue.get();
+    }
+
+    @Override
     public int getNumImages(boolean allowSearch) throws IOException {
-        return 0;
+        if (allowSearch) {
+            initialize(-1);
+            return 1;
+        }
+        return -1;
     }
 
     @Override
     public int getWidth(int imageIndex) throws IOException {
-        return 0;
+        validateImageIndex(imageIndex);
+
+        // this ImageReader isn't really focused on the image size, but it's trivial to identify, so...
+
+        initialize(-1);
+        return imageSize.width;
     }
 
     @Override
     public int getHeight(int imageIndex) throws IOException {
-        return 0;
+        validateImageIndex(imageIndex);
+
+        initialize(-1);
+        return imageSize.width;
     }
 
     @Override
-    public Iterator<ImageTypeSpecifier> getImageTypes(int imageIndex) throws IOException {
-        return null;
+    public Iterator<ImageTypeSpecifier> getImageTypes(int imageIndex) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    protected void processThumbnailStarted(int imageIndex, int thumbnailIndex) {
-        super.processThumbnailStarted(imageIndex, thumbnailIndex);
+    public IIOMetadata getStreamMetadata() {
+        throw new UnsupportedOperationException();
     }
 
     @Override
-    protected void processThumbnailProgress(float percentageDone) {
-        super.processThumbnailProgress(percentageDone);
-    }
-
-    @Override
-    protected void processThumbnailComplete() {
-        super.processThumbnailComplete();
-    }
-
-    @Override
-    protected void processThumbnailPassStarted(BufferedImage theThumbnail, int pass, int minPass, int maxPass, int minX, int minY, int periodX, int periodY, int[] bands) {
-        super.processThumbnailPassStarted(theThumbnail, pass, minPass, maxPass, minX, minY, periodX, periodY, bands);
-    }
-
-    @Override
-    protected void processThumbnailUpdate(BufferedImage theThumbnail, int minX, int minY, int width, int height, int periodX, int periodY, int[] bands) {
-        super.processThumbnailUpdate(theThumbnail, minX, minY, width, height, periodX, periodY, bands);
-    }
-
-    @Override
-    protected void processThumbnailPassComplete(BufferedImage theThumbnail) {
-        super.processThumbnailPassComplete(theThumbnail);
-    }
-
-    @Override
-    public IIOMetadata getStreamMetadata() throws IOException {
-        return null;
-    }
-
-    @Override
-    public IIOMetadata getImageMetadata(int imageIndex) throws IOException {
-        return null;
+    public IIOMetadata getImageMetadata(int imageIndex) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
     public BufferedImage read(int imageIndex, ImageReadParam param) throws IOException {
-        return null;
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -159,85 +277,36 @@ class JPEGThumbnailImageReader extends ImageReader {
     }
 
     @Override
-    public boolean hasThumbnails(int imageIndex) throws IOException {
-        return super.hasThumbnails(imageIndex);
+    public int getNumThumbnails(int imageIndex) throws IOException {
+        validateImageIndex(imageIndex);
+        initialize(-1);
+        return thumbnailDimensions.size();
     }
 
-    @Override
-    public int getNumThumbnails(int imageIndex) throws IOException {
-        return super.getNumThumbnails(imageIndex);
+    private void validateImageIndex(int imageIndex) {
+        if (imageIndex != 0)
+            throw new IndexOutOfBoundsException();
     }
 
     @Override
     public int getThumbnailWidth(int imageIndex, int thumbnailIndex) throws IOException {
-        return super.getThumbnailWidth(imageIndex, thumbnailIndex);
+        validateImageIndex(imageIndex);
+        initialize(-1);
+        return thumbnailDimensions.get(thumbnailIndex).width;
     }
 
     @Override
     public int getThumbnailHeight(int imageIndex, int thumbnailIndex) throws IOException {
-        return super.getThumbnailHeight(imageIndex, thumbnailIndex);
+        validateImageIndex(imageIndex);
+        initialize(-1);
+        return thumbnailDimensions.get(thumbnailIndex).height;
     }
 
     @Override
     public synchronized BufferedImage readThumbnail(int imageIndex,
                                        int thumbnailIndex) throws IOException {
-        if (input instanceof ImageInputStream) {
-            ImageInputStream iis = (ImageInputStream) input;
-            iis.reset();
-            AtomicReference<BufferedImage> returnValue = new AtomicReference<>();
-            JPEGMetaDataListener listener = new JPEGMetaDataListener() {
-                int thumbnailCtr = 0;
-
-                @Override
-                public boolean isThumbnailAccepted(String markerName, int width, int height) {
-                    return returnValue.get() == null;
-                }
-
-                @Override
-                public void addProperty(String markerName, String propertyName, Object value) {
-
-                }
-
-                @Override
-                public void addThumbnail(String markerName, BufferedImage bi) {
-                    if (thumbnailCtr == thumbnailIndex) {
-                        returnValue.set(bi);
-                    }
-                    thumbnailCtr++;
-                }
-
-                @Override
-                public void addComment(String markerName, String comment) {
-
-                }
-
-                @Override
-                public void startFile() {
-
-                }
-
-                @Override
-                public void endFile() {
-
-                }
-
-                @Override
-                public void imageDescription(int bitsPerPixel, int width, int height, int numberOfComponents) {
-
-                }
-
-                @Override
-                public void processException(Exception e, String markerCode) {
-
-                }
-            };
-            JPEGMetaData.read(new InputStreamConverter(iis), listener);
-            return returnValue.get();
-        } else {
-            // we shouldn't reach this condition, because calling setInput(Object)
-            // should have already thrown an exception
-            throw new IllegalStateException("unsupported input: " + input);
-        }
+        validateImageIndex(imageIndex);
+        return initialize(thumbnailIndex);
     }
 }
 
