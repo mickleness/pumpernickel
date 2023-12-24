8e7cb2326031f646ea425953a23d1765ce5ef8e3
jeremy <jeremy.wood@mac.com>
2023-12-23 20:22:53 -0500

    Updating source code headers

--- src/main/java/com/pump/image/jpeg/JPEGThumbnailImageReaderSpi.java
+++ src/main/java/com/pump/image/jpeg/JPEGThumbnailImageReaderSpi.java
@@ -1,3 +1,13 @@
+/**
+ * This software is released as part of the Pumpernickel project.
+ * 
+ * All com.pump resources in the Pumpernickel project are distributed under the
+ * MIT License:
+ * https://github.com/mickleness/pumpernickel/raw/master/License.txt
+ * 
+ * More information about the Pumpernickel project is available here:
+ * https://mickleness.github.io/pumpernickel/
+ */
 package com.pump.image.jpeg;
 
 import javax.imageio.ImageReader;
